5941a48953020c699ffb202d19e56c1a321c6643
viktorhc27 <victor.huanca1999@gmail.com>
2025-08-21 17:04:30 -0400

    fix

--- src/features/ruta/ruta.routes.js
+++ src/features/ruta/ruta.routes.js
@@ -1,11 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middlewares/auth');
+const tieneRole = require('../../middlewares/tieneRole');
 const { index, create, eliminar, show, update } = require('./ruta.controller');
 
-router.get('/index', index);
-router.post('/create', create);
-router.get('/view', show);
-router.put('update', update);
-router.delete('delete', eliminar);
+router.get('/index', [
+  /*   auth,
+    tieneRole('admin', 'cliente') */
+], index);
+router.post('/create', [
+   /*  auth,
+    tieneRole('admin') */
+], create);
+router.get('/view/:id', [
+    auth,
+    tieneRole('admin','cliente')
+], show);
+router.put('/update/:id', [
+  /*   auth,
+    tieneRole('admin') */
+], update);
+router.delete('/delete/:id', [
+ /*    auth,
+    tieneRole('admin') */
+], eliminar);
 
 module.exports = router;
